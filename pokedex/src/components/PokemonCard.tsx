87ca3b32b308631bd3d652e39f16bdc1814f951f
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-10-22 17:37:49 +0200

    avant derniere version

--- pokedex/src/components/PokemonCard.tsx
+++ pokedex/src/components/PokemonCard.tsx
@@ -1,12 +1,22 @@
 function PokemonCard()
-{
+{   const Pokemon = pokemonList[1];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+            {Pokemon.imgSrc === undefined ? <p>???</p> : <img src={Pokemon.imgSrc}/>}
+        <figcaption>{Pokemon.name}</figcaption>
         </figure>
     );
-
 }
   
   export default PokemonCard;
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
