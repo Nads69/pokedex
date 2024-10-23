a1bcb848aa9195e7397ba0bdcf1e066792a5a570
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-10-23 16:26:06 +0200

    fin de la quete 4

--- pokedex/src/components/PokemonCard.tsx
+++ pokedex/src/components/PokemonCard.tsx
@@ -1,22 +1,13 @@
-function PokemonCard()
-{   const Pokemon = pokemonList[1];
+function PokemonCard({pokemon})
+{
+
     return (
         <figure>
-            {Pokemon.imgSrc === undefined ? <p>???</p> : <img src={Pokemon.imgSrc}/>}
-        <figcaption>{Pokemon.name}</figcaption>
+            {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc}/>}
+        <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
+
 }
   
   export default PokemonCard;
-
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
