f3e846420da047391a09d600a237a5f2c63cc755
nadir haddouche <nads@MacBook-Pro-de-Nadir.local>
2024-10-24 16:29:01 +0200

    fin de quete 6

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
-    imageSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     name: "bulbasaur",
+    imageSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imageSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imageSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imageSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
@@ -13,10 +30,25 @@ const pokemonList = [
 
 
 function App() {
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickMoins = () => {
+    if (pokemonIndex > 0)
+      setPokemonIndex(pokemonIndex - 1)
+  };
+
+  const handleClickPlus = () => {
+    if (pokemonIndex < pokemonList.length-1)
+    setPokemonIndex(pokemonIndex + 1)
+  };
+
   return (
-    <>
-      <PokemonCard pokemon={pokemonList[0]}/>
-    </>
+    <div>
+      <PokemonCard  pokemon={pokemonList[pokemonIndex]}/>
+      <button onClick={handleClickMoins}>Previous</button>
+      <button onClick={handleClickPlus}>Next</button>
+    </div>
 
   );
 }
